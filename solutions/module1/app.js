f972fa5840b87b973167e34e9576bd8d97897b18
tglouis <tglouis@yahoo.com>
2017-08-05 16:56:00 -0500

    Updated solution

--- solutions/module1/app.js
+++ solutions/module1/app.js
@@ -2,25 +2,30 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function () {
+LunchCheckController.inject = ['$scope'];
 
-  function checkLunch($scope) {
+function LunchCheckController($scope) {
+  $scope.lunchEntry = "";
+  $scope.lunchMessage = "";
 
+  $scope.checkLunch = function() {
     var lunchItemsEntry = $scope.lunchEntry;
-    var lunchItems[] = lunchItemsEntry.split(',');
-    var lunchItemCount = 0;
+    var lunchItems = lunchItemsEntry.split(',').filter(item => item != "");
+    var lunchItemCount = lunchItems.length;
+    console.log(lunchItems);
 
-    lunchItems.forEach(function(lunchItem){
-
-        if (lunchItem.trim() <> '') {
-          lunchItemCount++;
-        }
-      }
-      return lunchItemCount;
-    });
-
-  }
-});
+    if (lunchItemCount == 0) {
+      $scope.lunchMessage = "Please enter data first";
+    }
+    else if (lunchItemCount > 3) {
+      $scope.lunchMessage = "Too much!";
+    }
+    else {
+      $scope.lunchMessage = "Enjoy!";
+    }
+  };
+}
 
 })();
